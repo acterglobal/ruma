6b3800b92c178ba949b6fd9d310f21fca78ce059
Benjamin Kampmann <ben@gnunicorn.org>
2024-10-20 15:41:17 +0100

    Empty previews

--- crates/ruma-events/src/room/message/url_preview.rs
+++ crates/ruma-events/src/room/message/url_preview.rs
@@ -31,7 +31,19 @@ pub struct PreviewImage {
     #[serde(rename = "og:image:type", skip_serializing_if = "Option::is_none")]
     pub mimetype: Option<String>,
 }
+impl PreviewImage {
+    /// Whether this is effectively empty
+    pub fn is_none(&self) -> bool {
+        self.url.is_none()
+    }
+    /// Whether this has any preview image information
+    pub fn is_some(&self) -> bool {
+        self.url.is_some()
+    }
+}
 
+/// Preview Information for a URL matched in the message text, according to
+/// [MSC 4095](https://github.com/matrix-org/matrix-spec-proposals/pull/4095)
 #[derive(Clone, Debug, Default, Deserialize, Serialize)]
 #[cfg_attr(not(feature = "unstable-exhaustive-types"), non_exhaustive)]
 pub struct UrlPreview {
@@ -51,9 +63,23 @@ pub struct UrlPreview {
     #[serde(rename = "og:description", skip_serializing_if = "Option::is_none")]
     pub description: Option<String>,
 
-    // extra fields
-    #[serde(flatten)]
-    pub image: Option<PreviewImage>,
+    /// Metadata of a preview image if given
+    #[serde(flatten, skip_serializing_if = "PreviewImage::is_none")]
+    pub image: PreviewImage,
+}
+
+impl UrlPreview {
+    /// Whether this preview is empty and thus the users homeserver should be
+    /// asked for preview data instead.
+    // According to the [MSC](https://github.com/beeper/matrix-spec-proposals/blob/bundled-url-previews/proposals/4095-bundled-url-previews.md)
+    // if there is a match but no fields (other than the matched_url), the client
+    // should fallback to asking the homeserver
+    pub fn should_ask_homeserver(&self) -> bool {
+        self.url.is_none()
+            && self.title.is_none()
+            && self.description.is_none()
+            && self.image.is_none()
+    }
 }
 
 #[cfg(test)]
@@ -98,8 +124,8 @@ mod tests {
         assert_eq!(url.as_ref().unwrap(), "https://matrix.org/");
 
         // Check the preview image parsed:
-
-        let PreviewImage { size, url, height, width, mimetype } = image.clone().unwrap();
+        assert!(image.is_some());
+        let PreviewImage { size, url, height, width, mimetype } = image.clone();
         assert_eq!(u64::from(size.unwrap()), 16588);
         assert_eq!(
             url.clone().unwrap().to_string(),
@@ -108,6 +134,42 @@ mod tests {
         assert_eq!(u64::from(height.unwrap()), 400);
         assert_eq!(u64::from(width.unwrap()), 800);
         assert_eq!(mimetype, Some("image/jpeg".to_owned()));
+    }
+
+    #[test]
+    fn parsing_example_no_previews() {
+        let normal_preview = json!({
+                      "msgtype": "m.text",
+                      "body": "https://matrix.org",
+                      "m.url_previews": [],
+                      "m.mentions": {}
+        });
+
+        let message_with_preview: TextMessageEventContent = from_value(normal_preview).unwrap();
+        let TextMessageEventContent { url_previews, .. } = message_with_preview;
+        assert!(url_previews.clone().unwrap().is_empty(), "Unexpectedly found url previews");
+    }
+
+    #[test]
+    fn parsing_example_empty_previews() {
+        let normal_preview = json!({
+                "msgtype": "m.text",
+                "body": "https://matrix.org",
+                "m.url_previews": [
+                  {
+                    "matrix:matched_url": "https://matrix.org"
+                  }
+                ],
+                "m.mentions": {}
+        });
+
+        let message_with_preview: TextMessageEventContent = from_value(normal_preview).unwrap();
+        let TextMessageEventContent { url_previews, .. } = message_with_preview;
+        let previews = url_previews.expect("No url previews found");
+        assert_eq!(previews.len(), 1);
+        let preview = previews.first().unwrap();
+        assert_eq!(preview.matched_url, "https://matrix.org");
+        assert!(preview.should_ask_homeserver());
     }
 
     #[test]
@@ -139,7 +201,9 @@ mod tests {
         let MessageEventContent { url_previews, .. } = message_with_preview;
         let previews = url_previews.expect("No url previews found");
         assert_eq!(previews.len(), 1);
-        let UrlPreview { image, matched_url, title, url, description } = previews.first().unwrap();
+        let preview = previews.first().unwrap();
+        assert!(!preview.should_ask_homeserver());
+        let UrlPreview { image, matched_url, title, url, description } = preview;
         assert_eq!(matched_url, "matrix.org/support");
         assert_eq!(title.as_ref().unwrap(), "Support Matrix");
         assert_eq!(
@@ -149,8 +213,8 @@ mod tests {
         assert_eq!(url.as_ref().unwrap(), "https://matrix.org/support/");
 
         // Check the preview image parsed:
-
-        let PreviewImage { size, url, height, width, mimetype } = image.clone().unwrap();
+        assert!(image.is_some());
+        let PreviewImage { size, url, height, width, mimetype } = image.clone();
         assert_eq!(u64::from(size.unwrap()), 16588);
         assert_eq!(
             url.clone().unwrap().to_string(),
